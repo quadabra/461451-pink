4d22d7e5464bded0418f2879ef1187c02ce49a47
quadabra <gr.maks@gmail.com>
2017-09-30 00:03:22 +0300

    add some styles

---
+++ js/nav-list.js
@@ -0,0 +1,10 @@
+var navButton = document.querySelector('.site-navigation__toggle');
+var navList = document.querySelector('.site-navigation__list');
+var navCont = document.querySelector('.site-navigation__container');
+navList.classList.add('site-navigation__list_hidden');
+navCont.classList.add('site-navigation__container_minimized');
+navButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  navList.classList.toggle('site-navigation__list_hidden');
+  navCont.classList.toggle('site-navigation__container_minimized');
+});
